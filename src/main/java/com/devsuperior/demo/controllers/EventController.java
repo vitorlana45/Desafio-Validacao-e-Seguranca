d2147067fefa5479d367d41204ad9fd44018ac54
vitorlana45 <vitoresmerio00@gmail.com>
2024-03-16 23:10:33 -0300

    Event controller, service and repository created

---
+++ src/main/java/com/devsuperior/demo/controllers/EventController.java
@@ -0,0 +1,13 @@
+package com.devsuperior.demo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/events")
+public class EventController {
+
+
+
+
+}
---
+++ src/main/java/com/devsuperior/demo/services/EventService.java
@@ -0,0 +1,8 @@
+package com.devsuperior.demo.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class EventService {
+
+}
