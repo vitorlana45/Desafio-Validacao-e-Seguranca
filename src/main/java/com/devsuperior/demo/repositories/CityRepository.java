4bdaf24aa7fa1413e9fd8f083ebdc65aa1d82682
vitorlana45 <vitoresmerio00@gmail.com>
2024-03-16 21:59:09 -0300

    removing security dependencies and OAUTH2 temporarily

--- src/main/java/com/devsuperior/demo/repositories/CityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.devsuperior.demo.repositories;
-
-import com.devsuperior.demo.entities.City;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CityRepository extends JpaRepository<City, Long> {
-}
